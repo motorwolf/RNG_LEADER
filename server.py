c80632246b79d9ff1350368d886dff388583d32d
Kristen Payne <victorycabbage@gmail.com>
2019-07-05 19:49:41 -0700

    Add track modifications again

--- server.py
+++ server.py
@@ -327,6 +327,7 @@ def logged_in_and_auth(id_to_check):
 
 def connect_to_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
 
